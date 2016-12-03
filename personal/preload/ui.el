48ba706a90d9cbee49b4f3c53fcfd37d1fdeabbe
Cliff McIntosh <cmcintosh@opinionlab.com>
2016-12-03 15:41:56 -0600

    re-copy from prelude original project

---
+++ personal/preload/ui.el
@@ -0,0 +1,16 @@
+;;; package ui customizations
+
+;;; Code:
+
+;;; LightTable-like theme
+;;; (setq prelude-theme 'noctilux)
+
+(setq prelude-theme 'monokai)
+
+;;; Favorite IDEA-like theme so far
+;;; (setq prelude-theme 'darcula)
+
+;;; (setq prelude-theme 'idea-darkula)
+
+(provide 'ui)
+;;; ui.el ends here
